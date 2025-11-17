b6aac08fb1e52a6d7e4f1fefef2f17f123c3c9c1
ShedrachIkenna <Shedrach686@gmail.com>
2025-11-17 01:24:20 +0100

    added CPU/pilot-friendly settings

--- build_token_pos_map_cpu.py
+++ build_token_pos_map_cpu.py
@@ -12,3 +12,8 @@ from datasets import load_dataset
 from transformers import GPT2TokenizerFast
 import spacy 
 from tqdm import tqdm
+
+# CPU/pilot-friendly settings 
+NUM_WORDS_TO_PROCESS = 50_000 
+OUTPATH = "token_pos_map.json" # output file 
+MIN_OCCURENCES = 3 # if a token occurs fewer than 3 times in the entire dataset, POS tag won't be assigned 
