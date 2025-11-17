3f0aba5ccbdc44e848891a74654b3197ef2f4c37
ShedrachIkenna <Shedrach686@gmail.com>
2025-11-17 01:37:41 +0100

    added build_token_pos_mapping function signature, parameters and defaults

--- build_token_pos_map_cpu.py
+++ build_token_pos_map_cpu.py
@@ -17,3 +17,13 @@ from tqdm import tqdm
 NUM_WORDS_TO_PROCESS = 50_000 
 OUTPATH = "token_pos_map.json" # output file 
 MIN_OCCURENCES = 3 # if a token occurs fewer than 3 times in the entire dataset, POS tag won't be assigned 
+
+def build_token_pos_mapping(
+        nun_words: int = NUM_WORDS_TO_PROCESS, 
+        output_path: str = OUTPATH,
+        min_occurences: int = MIN_OCCURENCES, 
+        dataset_name: str = "wikitext",
+        dataset_config: str = "wikitext-2-raw-v1",
+        tokenizer_name: str = "gpt2",
+        spacy_model: str = "en_core_web_sm"
+)
