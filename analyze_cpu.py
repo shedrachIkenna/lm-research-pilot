5739aa893544edbd18b3d0cb31c8915bdbc1736f
ShedrachIkenna <Shedrach686@gmail.com>
2025-11-22 05:41:25 +0100

    suppress warnings from appearing in the console

--- analyze_cpu.py
+++ analyze_cpu.py
@@ -23,3 +23,10 @@ import torch
 import umap 
 import warnings 
 from transformers import GPT2TokenizerFast, GPT2LMHeadModel
+
+
+warnings.filterwarnings('ignore', category=UserWarning) # Suppress user warning from appearing in the console 
+warnings.filterwarnings('ignore', category=FutureWarning) # Suppress Future warnings about a library from cluttering the output in the console 
+torch.set_num_threads(2) # Limit pytorch to using only 2 CPU threads for parallel operations (like matrix multiplication)
+
+
